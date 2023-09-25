2870b08975ec2981852fc1e1563dc323fef84bfc
raviasaini <ravia.saini81@outlook.com>
2023-09-25 17:16:30 +0100

    profile page code split into components

---
+++ client/src/components/Buttons/index.jsx
@@ -0,0 +1,11 @@
+const Buttons = () => {
+	return (
+		<div className="div-wrapper">
+			<div className="text-wrapper-13">
+				<button className="edit-button">Edit Profile</button>
+			</div>
+		</div>
+	);
+}
+
+export default Buttons
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as Header } from './Header'
+export { default as ProfileInfo } from './ProfileInfo'
+export { default as Buttons } from './Buttons'
+export { default as Stats } from './Stats'
