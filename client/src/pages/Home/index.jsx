c3794713f56d3e455a8872b999bc8d755da096ed
raviasaini <ravia.saini81@outlook.com>
2023-09-25 23:34:24 +0100

    basic homepage design made

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import "../../assets/css/home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="index">
+      <div className="completion-bar">
+        <div className="completion" />
+      </div>
+      <div className="images-container">
+        <div className="images">
+          <img
+            className="image1"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="image2"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="image3"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="image4"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="image5"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="image6"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-1@2x.png"
+          />
+          <img
+            className="character"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/651165e23f4e55995d9af710/releases/651200163f4e55995d9af7ea/img/image-7@2x.png"
+          />
+        </div>
+      </div>
+  </div>
   )
 }
 
