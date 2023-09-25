0140ad4f80e850577deb47e3018f251b124d75db
Cams-Plan <ca.francis@outlook.com>
2023-09-25 18:11:14 +0100

    added the footer and founders components (blank) and added the first item of the missionStatement list

---
+++ client/src/components/Founders/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Founders = () => {
+  return (
+    <div>Founders</div>
+  )
+}
+
+export default Founders
--- client/src/pages/About/index.jsx
+++ client/src/pages/About/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { Founders } from '../../components'
+
 const About = () => {
+
+  const missionStatement = [
+    "Welcome to your new virtual wellness space at WellSpace. Our mission is build a self-care app that tackles the issue of user drop-off that motivates our users to complete small wellness tasks each day, with the overall message that wellness doesn't have to be large efforts, but instead can be small managable intentions that are fulfilled daily for an instant sense of reward and genuine benefit to your health.",
+    ""
+  ]
+
   return (
-    <div>About</div>
+    <>
+    <h1>About WellSpace</h1>
+    <h2>Our Mission</h2>
+    <p></p>
+    </>
   )
 }
 
