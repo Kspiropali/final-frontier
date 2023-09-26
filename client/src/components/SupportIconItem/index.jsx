b0a4a44cebfc4a2640f417abc4c8158762ad8298
Cams-Plan <ca.francis@outlook.com>
2023-09-26 08:00:33 +0100

    created support page style sheet

---
+++ client/src/components/SupportIconItem/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SupportIconItem = () => {
+
+
+    // gather the data for the support services (json or db)
+    // icon link for icon
+
+
+  return (
+    <>
+    <div className='support-item-container' >
+        <div className='support-tile'>
+            <img src="" alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default SupportIconItem
