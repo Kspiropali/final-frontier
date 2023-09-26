3fe4aa02da4aefe627400d1cb854dd8301145ce3
Cams-Plan <ca.francis@outlook.com>
2023-09-26 13:01:39 +0100

    added the alignments for the service box

--- client/src/components/SupportServicesList/index.jsx
+++ client/src/components/SupportServicesList/index.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const SupportServicesList = ({supportServices}) => {
   return (
-    <div>SupportServicesList</div>
+    <>
+    <div className='center-cont'>
+      <h3 className='icon-title yellow-text'> Icon Title</h3>
+      <div className='service-options-container'>
+        <div className="service-item">
+
+          <div className='service-img'>
+            <img src="" alt="" />
+          </div>
+          <div className='service-text-area'>
+            <p></p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
