894bd7c4213c7c2762f95517904d92707ff8eb50
Cams-Plan <ca.francis@outlook.com>
2023-09-26 10:16:08 +0100

    added the useState hooks for first and rest of icons + added them as hooks to support icons component

--- client/src/pages/Support/index.jsx
+++ client/src/pages/Support/index.jsx
@@ -1,18 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import '../../assets/css/support.css'
 import { SupportIcons, SupportServicesList } from '../../components'
 
 const Support = () => {
+  //this will be a number
+  const userMood = ""
 
+  const supportServiceSchema = [
+    {
+      id: 0,
+      type_name: "talking",
+      type_id: 1,
+      icon_url: "",
+      service_list: [{title: "Talking Service", url: "https://www.google.com/search?q=talk+to+someone"}]
+    },
+    {
+      id: 1,
+      type_name: "stress management",
+      type_id: 2,
+      icon_url: "",
+      service_list: [{title: "Stress Management Service", url: "https://www.google.com/search?q=stress+management"}]
+    },
+    {
+      id: 2,
+      type_name: "physical wellness",
+      type_id: 3,
+      icon_url: "",
+      service_list: [{title: "Physical Wellness Service", url: "https://www.google.com/search?q=stress+management"}]
+    },
+    {
+      id: 3,
+      type_name: "emotional support",
+      type_id: 4,
+      icon_url: "",
+      service_list: [{title: "Physical Wellness Service", url: "https://www.google.com/search?q=stress+management"}]
+    }
+  ]
 
-  //use useState to control the selected service
-  const [supportServices, setSupportServices] = useState("")
+  //the first position is dependent on the mood provided by the user or defaults to the first in the index of the support service schema
 
+  const [firstPosition, setFirstPosition] = useState(userMood ? findService(userMood) : supportServiceSchema[0])
+
+  const [supportServices, setSupportServices] = useState(supportServiceSchema)
+
+  function findService(moodNum){
+    //returns an array object (must be accessed via index [0])
+    return supportServiceSchema.filter((service) => service.type_id == moodNum)
+  }
+  useEffect(() => {
+    
+  }, [])
+  console.log(firstPosition)
   return (
     <>
-    <SupportIcons/>
+    <SupportIcons firstPosition={firstPosition} supportServices={supportServices} />
     <SupportServicesList/>
     </>
   )
