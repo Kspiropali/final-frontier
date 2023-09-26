b322bbb0197835ae44ec9aa4196ee53cccaa2f6a
Cams-Plan <ca.francis@outlook.com>
2023-09-26 08:55:19 +0100

    setup the icons for future login

--- client/src/components/SupportIcons/index.jsx
+++ client/src/components/SupportIcons/index.jsx
@@ -9,10 +9,19 @@ const SupportIcons = () => {
 
   return (
     <>
-    <div className='support-item-container' >
-        <div className='support-tile'>
-            <img src="" alt="" />
-        </div>
+    <div className='all-icons-container'>
+      {/* This one will be reserved for the first index OR the service that corresponds most to the user's mood */}
+      <div className='support-item-container' >
+          <div className='support-tile'>
+              <img src="" alt="" />
+          </div>
+      </div>
+      {/* this will be the rest of the icons which will be mapped based on the list of service categories */}
+      <div className='support-item-container' >
+          <div className='support-tile'>
+              <img src="" alt="" />
+          </div>
+      </div>
     </div>
     </>
   )
