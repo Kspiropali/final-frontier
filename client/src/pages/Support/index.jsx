e5c00f62fb9991e16d4ae2df8cd0eb5a7d58e83a
Cams-Plan <ca.francis@outlook.com>
2023-09-26 08:02:25 +0100

    added the import for support icon temporarily into support page

--- client/src/pages/Support/index.jsx
+++ client/src/pages/Support/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+
+import '../../assets/css/support.css'
+import { SupportIconItem } from '../../components'
+
 const Support = () => {
   return (
     <div>Support</div>
