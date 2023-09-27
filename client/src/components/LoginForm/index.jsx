b7da237939d6274cda43356bc817e3aa41078f3f
Cams-Plan <ca.francis@outlook.com>
2023-09-27 13:23:33 +0100

    added email username and password to the registration form. and some field border styling for no-match & correct syntax. Just need to do one for incorrect syntax

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
 
 const LoginForm = () => {
+
+    const handleUsername = () => {
+        return
+    }
+    const handleInitialPassword = () => {
+        return
+    }
+    const handleConfirmationPassword = () => {
+        return
+    }
+
   return (
-    <div>LoginForm</div>
+    <>
+    <form
+        aria-label='login form'
+        role="login"
+        // onSubmit={handleSubmit}
+      > 
+        {/* <label>Username: </label> */}
+        <input
+          type="text"
+          id="username"
+        //   onChange={handleUsername}
+        //   value="username"
+          placeholder='username'
+          required
+         className='input-field'/>
+         
+        {/* <label>Email: </label> */}
+        <input
+          type="text"
+          id="email"
+        //   onChange={handleUsername}
+        //   value="username"
+          placeholder='email'
+          required
+         className='input-field'/>
+
+        {/* <label>Confirm Email: </label> */}
+        <input
+          type="text"
+          id="confirm-email"
+        //   onChange={handleUsername}
+        //   value="username"
+          placeholder='confirm email'
+          required
+         className='input-field'/>
+
+        {/* <label>Email: </label> */}
+        <input
+          type="text"
+          id="password"
+        //   onChange={handleUsername}
+        //   value="username"
+          placeholder='password'
+          required
+         className='input-field'/>
+
+        {/* <label>Confirm Email: </label> */}
+        <input
+          type="text"
+          id="confirm-password"
+        //   onChange={handleUsername}
+        //   value="username"
+          placeholder='confirm password'
+          required
+         className='input-field no-match'/>
+
+        {/* <label>Password: </label>
+        <input
+          type="password"
+          id="password"
+          onChange={handlePassword}
+        //   value={password}
+          placeholder='password'
+          required
+        /> */}
+
+        {/* <input className='login' type="submit" value="Login" />
+        {message && <p>{message}</p>} */}
+
+      </form>
+    </>
   )
 }
 
