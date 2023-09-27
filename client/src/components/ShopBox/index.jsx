64571e4530d7f1b218d67d966a115cfc19ab4c54
nplsam <psammerrick@gmail.com>
2023-09-27 16:12:38 +0100

    placed toggle button in the correct position

--- client/src/components/ShopBox/index.jsx
+++ client/src/components/ShopBox/index.jsx
@@ -5,9 +5,11 @@ import '../../assets/css/shopbox.css'
 const ShopBox = () => {
   return (
     <>
+    <div className="shop-box-container">
      <h1 className="shop-header">Item Shop</h1>
-     <div>
+     <div className="toggle-buttons-container">
         <ToggleShop />
+     </div>
      </div>
     <div className="box"></div>
     </>
