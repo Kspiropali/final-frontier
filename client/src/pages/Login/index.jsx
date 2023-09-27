6304b0c168e84c237142cb32760d7e0537df3e4c
Cams-Plan <ca.francis@outlook.com>
2023-09-27 11:39:17 +0100

    added the toggle buttons to the login page and created the list of titles to go into container title h2

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import "../../assets/css/login.css";
 
 const Login = () => {
+
+  const containerTitle = ["Return To Your WellSpace", "Create Your Own WellSpace", "Reset Password"]
+
   return (
     <>
     <h1 className='top-header yellow-text'>WellSpace</h1>
     {/* Container for the Login compents */}
-    <div className='login-container'></div>
+    <div className='login-container'>
+      <div className='log-reg-btn-container'>
+        <button className='white-text log-reg-btn active-log-btn'>Login</button>
+        <button className='white-text log-reg-btn'>Register</button>
+      </div>
+      <h2 className='log-reg-title'>fff</h2>
+    </div>
     </>
   )
 }
