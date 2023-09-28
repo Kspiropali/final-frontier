93c4d3798d7147bc18f39130e2fa9ff2bdf96942
Cams-Plan <ca.francis@outlook.com>
2023-09-28 13:00:08 +0100

    OAuth buttons added to the form

--- client/src/components/OAuthButtons/index.jsx
+++ client/src/components/OAuthButtons/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import google from "../../assets/images/oAuthIcons/google.png"
+import facebook from "../../assets/images/oAuthIcons/facebook.png"
 
 const OAuthButtons = () => {
   return (
     <>
     <button className='OAuth-btn' id='google-Oauth'>
-      <img src="" alt="" />Login with Google
+      <img src={google} alt="" className='OAuth-icon'/>Login with Google
+    </button>
+    <button className='OAuth-btn' id='google-Oauth'>
+      <img src={facebook} alt="" className='OAuth-icon'/>Login with Facebook
     </button>
     </>
   )
