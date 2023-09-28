0072be08d92f1b74f781b7c89ffe1fa201bbf1b3
nplsam <psammerrick@gmail.com>
2023-09-28 10:47:28 +0100

    created iteminfo component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -27,3 +27,5 @@ export { default as ToggleShop } from './ToggleShop'
 export { default as CoinBalance } from './CoinBalance'
 export { default as Timer } from './Timer'
 export { default as ShopItem } from './ShopItem'
+export { default as ItemInfo } from './ItemInfo'
+
