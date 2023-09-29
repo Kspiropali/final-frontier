a705ca3dec709892e203f903058b7c1513785a64
raviasaini <ravia.saini81@outlook.com>
2023-09-29 16:03:26 +0100

    random commit to pull

--- client/src/components/BackgroundImage/index.jsx
+++ client/src/components/BackgroundImage/index.jsx
@@ -3,15 +3,15 @@ import { useTaskContext } from '../../contexts/TaskContext';
 const BackgroundImage = () => {
   const { completionPercentage } = useTaskContext();
   return (
-    <div className="bg-img-wrapper"         style={{ 
-        width: `${completionPercentage}%`, 
-        height: `${completionPercentage}%` 
-        }}>
-        <img 
-        className="backgroundim"
-        src="/src/assets/images/testbg/rainbow.jpg" 
-        />
-      </div>
+    <div className="bg-img-wrapper" style={{ 
+			width: `${completionPercentage}%`, 
+			height: `${completionPercentage}%` 
+			}}>
+			<img 
+			className="backgroundim"
+			src="/src/assets/images/testbg/rainbow.jpg" 
+			/>
+    </div>
   );
 };
 
