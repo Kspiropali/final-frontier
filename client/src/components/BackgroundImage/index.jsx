4b17c379ee32af3729544518a688259934b60526
raviasaini <ravia.saini81@outlook.com>
2023-10-02 00:24:11 +0100

    growing background works

--- client/src/components/BackgroundImage/index.jsx
+++ client/src/components/BackgroundImage/index.jsx
@@ -1,18 +1,33 @@
 import { useTaskContext } from '../../contexts/TaskContext';
 
 const BackgroundImage = () => {
+
   const { completionPercentage } = useTaskContext();
+
+  const progress = completionPercentage;  
+
+  const maxRadius = 50;
+
+  const radius = maxRadius - (maxRadius * progress) / 100;
+
+  const borderRadius = `${radius}%`;
+
   return (
-    <div className="bg-img-wrapper" style={{ 
-			width: `${completionPercentage}%`, 
-			height: `${completionPercentage}%` 
-			}}>
-			<img 
-			className="backgroundim"
-			src="/src/assets/images/testbg/rainbow.jpg" 
-			/>
+    <div 
+      className="bg-image-wrapper"
+      style={{
+        borderRadius
+      }} 
+    >
+      <img 
+        className="backgroundim"
+        src="/src/assets/images/testbg/rainbow.jpg" 
+      />
     </div>
   );
+
 };
 
 export default BackgroundImage;
+
+
