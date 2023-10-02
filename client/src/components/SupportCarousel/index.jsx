b0f294089cbf1604d7d9dfdfc90e15d97af9d670
Cams-Plan <ca.francis@outlook.com>
2023-10-02 20:35:15 +0100

    carousel items on. just need to work on the alignment

--- client/src/components/SupportCarousel/index.jsx
+++ client/src/components/SupportCarousel/index.jsx
@@ -7,19 +7,24 @@ import chevronLeft from "../../assets/images/supportIcons/chevronLeft.svg"
 import chevronRight from "../../assets/images/supportIcons/chevronRight.svg"
 import { SupportItem } from '../../components'
 
+import Carousel from 'react-bootstrap/Carousel';
 const SupportCarousel = ({currentSelection, supportServiceSchema, supportServices}) => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const updateIndex = (newIndex) => {
-    if (newIndex < 0){
-        newIndex = 0;
-    } else if (newIndex >= supportServiceSchema.length){
-        newIndex = supportServiceSchema.length -1;
-    }
+  // const updateIndex = (newIndex) => {
+  //   if (newIndex < 0){
+  //       newIndex = 0;
+  //   } else if (newIndex >= supportServiceSchema.length){
+  //       newIndex = supportServiceSchema.length -1;
+  //   }
 
-    setCurrentIndex(newIndex);
-  }
+  //   setCurrentIndex(newIndex);
+  // }
+
+  const handleSelect = (selectedIndex) => {
+    setCurrentIndex(selectedIndex);
+  };
 
   /*
   const sliderStyles = {
@@ -55,6 +60,20 @@ const SupportCarousel = ({currentSelection, supportServiceSchema, supportService
 
   return (
     <>
+    <Carousel activeIndex={currentIndex} onSelect={handleSelect} interval={null}>
+      {supportServiceSchema.map((service, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <SupportItem service={service} currentSelection={currentSelection} />
+            <Carousel.Caption>
+              <h3 className='yellow-text'>{service.type_name}</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+        
+      })}
+      
+    </Carousel>
     
 
         {/* {supportServices.map((service, index) => {
