f226a38fc6c24ed84720083c7b79fbe64850cefa
nplsam <psammerrick@gmail.com>
2023-10-03 09:45:18 +0100

    item endpoints working

--- backend/app/routes/item_routes.py
+++ backend/app/routes/item_routes.py
@@ -9,13 +9,13 @@ item_bp = Blueprint('item', __name__)
 def list_items():
     items = get_items()
     print(item for item in items)
-    return jsonify({'items': [{'id': item.id, 'name': item.name, 'type': item.type, 'description': item.description, 'price': item.price} for item in items]})
+    return jsonify({'items': [{'id': item.id, 'name': item.name, 'type': item.type, 'description': item.description, 'price': item.price, 'image': item.image} for item in items]})
 
 # Route to fetch details of a specific item
 @item_bp.route('/<int:item_id>', methods=['GET'])
 def get_one_item(item_id):
     try:
         item = get_item(item_id)
-        return jsonify({'items': [{'id': item.id, 'name': item.name, 'type': item.type, 'description': item.description, 'price': item.price}]})
+        return jsonify({'items': [{'id': item.id, 'name': item.name, 'type': item.type, 'description': item.description, 'price': item.price, 'image': item.image}]})
     except:
         return "FAILED!"
