481505bae52e5f5979ac75fedda5d8d4444b2bab
Cams-Plan <ca.francis@outlook.com>
2023-10-04 19:17:34 +0100

    finished the about page 80%

--- client/src/components/AboutCarousel/index.jsx
+++ client/src/components/AboutCarousel/index.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import '../../assets/css/about.css'
 
 import Carousel from 'react-bootstrap/Carousel';
-const AboutCarousel = ({currentSelection}) => {
+const AboutCarousel = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -11,24 +11,28 @@ const AboutCarousel = ({currentSelection}) => {
     setCurrentIndex(selectedIndex);
   };
 
+  const missionStatement = [
+    ["To Improve Wellness App Engagement and Make Daily Self-Care Achieveable","Welcome to your new virtual wellness space at WellSpace. Our mission is to build a self-care app that tackles the issue of user drop-off and motivates our users to complete small wellness tasks each day, with the overall message that wellness doesn't have to be large efforts, but instead can be small managable intentions that are fulfilled daily."],
+
+    ["Using Gameification and Rewards to Keep Things Interesting","Our app achieves this by providing a combination of set wellness tasks and mystery tasks for users to complete each day. As users complete their tasks, they are rewarded in 3 ways:\nA homepage that brightens with each task completed\nCoins that can be redeemed in the WellSpace shop to decorate your avater and WellSpace\nPhysical and mental wellness benefits"],
+
+    ["Helping Users Achieve The Satisfaction of Consistency AND Self-care Exploration","Our combination of set and mystery tasks was done intentionally, so users can develop their core consistency through their set tasks, but also keep things fresh with mystery tasks that give them the opportunity to try other wellness activities they might not usually gravitate towards.", "We hope for our users to feel a sense of pride and accomplishment everytime they enter their WellSpace and see the new furnishings and personalisations, reflective of their consistent daily wellness habits."]
+  ]
+
   return (
     <>
-    <Carousel activeIndex={currentIndex} onSelect={handleSelect} interval={null} keyboard touch data-bs-theme="dark">
-      {/* {supportServiceSchema.map((service, index) => {
+    <Carousel activeIndex={currentIndex} onSelect={handleSelect} interval={7000} keyboard touch data-bs-theme="dark" className='about-mission-carousel' controls={false} pause='hover'>
+      {missionStatement.map((paragraph, index) => {
         return (
           <Carousel.Item key={index}>
-            <SupportItem service={service} currentSelection={currentSelection} setIsOpen={setIsOpen} open={isOpen}/>
-            <Carousel.Caption>
-              <h3 className='yellow-text'>{service.type_name}</h3>
-              <button className='support-carousel-btn'>Find {service.type_name} Services</button>
-            </Carousel.Caption>
-                      
+            <strong className='mission-carousel-strong-text'>{paragraph[0]}</strong>
+            <p className='mission-carousel-text'>{paragraph[1]}</p>           
           </Carousel.Item>
         )
-      })} */}
+      })}
     </Carousel>   
     </>
   )
 }
 
-export default AboutCarousel
+export default AboutCarousel;
